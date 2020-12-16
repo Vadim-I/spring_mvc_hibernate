796ed4800339ccd2e700efe17f10786573a2a1b1
Vadim-I <isaeff@bk.ru>
2020-12-16 23:31:01 +0700

    added form to delete employee

--- src/main/java/org/spring/mvc_hibernate/dao/EmployeeDAOImpl.java
+++ src/main/java/org/spring/mvc_hibernate/dao/EmployeeDAOImpl.java
@@ -2,6 +2,7 @@ package org.spring.mvc_hibernate.dao;
 
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
+import org.hibernate.query.Query;
 import org.spring.mvc_hibernate.entity.Employee;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
@@ -34,5 +35,13 @@ public class EmployeeDAOImpl implements EmployeeDAO {
         Session session = sessionFactory.getCurrentSession();
         Employee employee = session.get(Employee.class, id);
         return employee;
+    }
+
+    @Override
+    public void deleteEmployee(int id) {
+        Session session = sessionFactory.getCurrentSession();
+        Query<Employee> query = session.createQuery("delete from Employee where id =:employeeId");
+        query.setParameter("employeeId", id);
+        query.executeUpdate();
     }
 }
