62ed52182ed171ce00b3ef6ecb13624f33c93f05
Vadim-I <isaeff@bk.ru>
2020-12-16 19:18:55 +0700

    added form to save employee

--- src/main/java/org/spring/mvc_hibernate/controller/MyControlller.java
+++ src/main/java/org/spring/mvc_hibernate/controller/MyControlller.java
@@ -6,6 +6,7 @@ import org.spring.mvc_hibernate.service.EmployeeService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.List;
@@ -23,6 +24,22 @@ public class MyControlller {
         model.addAttribute("allEmps", allEmployees);
 
         return "all-employees";
+    }
+
+    @RequestMapping("/addNewEmployee")
+    public String addNewEmployee(Model model) {
+
+        Employee employee = new Employee();
+        model.addAttribute("employee", employee);
+        return "employee-info";
+    }
+
+    @RequestMapping("/saveEmployee")
+    public String saveEmployee(@ModelAttribute("employee") Employee employee) {
+
+        employeeService.saveEmployee(employee);
+
+        return "redirect:/";
     }
 
 }
